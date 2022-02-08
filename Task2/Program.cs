02ea10a46e13f5ead3856e1fe261289ebf94bda0
Aleksandr <kino_mac@mail.ru>
2022-02-09 00:21:06 +0300

    Добавлено решение 2-ой задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 12: Удалить вторую цифру трёхзначного числа.
+
+int GetLastDigit(int number)
+{
+    int result = number % 10;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+int thirdDigit = GetLastDigit(number);
+int firstDigit = number / 100;
+int result = firstDigit*10 + thirdDigit;
+Console.WriteLine($"Трехзначное число - {number}, итоговое число - {result}");
