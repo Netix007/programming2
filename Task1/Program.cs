f224dcee4486f181589062297c5e9894081f91ec
Aleksandr <kino_mac@mail.ru>
2022-02-08 23:56:56 +0300

    Добавлено решение 1-ой задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+
+int GetLastDigit(int number)
+{
+    int result = number % 10;
+    return result;
+} 
+
+int number = new Random().Next(100,1000);
+int result = number / 10;
+result = GetLastDigit(result);
+Console.WriteLine($"Вторая цифра числа {number} - {result}");
