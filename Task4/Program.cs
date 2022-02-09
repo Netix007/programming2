490d4a887a9e2ef1fd71bf70805111a69d5d68cc
Aleksandr <kino_mac@mail.ru>
2022-02-10 00:37:13 +0300

    Добавлено решение 4-ой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 16: Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+
+int number = new Random().Next(1,8);
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"{number}-ой день недели - выходной день");
+}
+else
+{
+    Console.WriteLine($"{number} - рабочий день");
+}
